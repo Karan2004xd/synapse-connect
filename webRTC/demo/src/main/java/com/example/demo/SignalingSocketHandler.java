b72190086c28d10cf174e8a0d616831d433099e1
Karan-Lenovo-Laptop <prajapatikaran2004@gmail.com>
2024-06-13 17:13:56 +0530

    learning webRTC

---
+++ webRTC/demo/src/main/java/com/example/demo/SignalingSocketHandler.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class SignalingSocketHandler extends TextWebSocketHandler {
+  private final Map<String, WebSocketSession> sessions = new HashMap<>();
+
+  @Override
+  public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    sessions.put(session.getId(), session);
+  }
+
+  @Override
+  protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    for (WebSocketSession s : sessions.values()) {
+      if (!s.getId().equals(s.getId())) {
+        s.sendMessage(message);
+      }
+    }
+  }
+
+  @Override
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    sessions.remove(session.getId());
+  }
+}
