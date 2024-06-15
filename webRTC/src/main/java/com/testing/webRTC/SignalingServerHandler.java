5bdd61a50dbea92588f2478b191ad0fb1b8954d5
Karan2004xd <prajapatikaran2004@gmail.com>
2024-06-16 03:25:30 +0530

    almost done with webRTC implementation testing

--- webRTC/src/main/java/com/testing/webRTC/SignalingServerHandler.java
+++ webRTC/src/main/java/com/testing/webRTC/SignalingServerHandler.java
@@ -1,5 +1,8 @@
 package com.testing.webRTC;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,24 +11,94 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class SignalingServerHandler extends TextWebSocketHandler {
   private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+  private final Map<String, Offer> offers = new ConcurrentHashMap<>();
+  private final Map<String, List<String>> iceCandidates = new ConcurrentHashMap<>();
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
     String payload = message.getPayload();
-    for (WebSocketSession session2 : sessions.values()) {
-      if (!session2.getId().equals(session.getId())) {
-        session2.sendMessage(new TextMessage(payload));
+    Map<String, String> msg = objectMapper.readValue(payload, Map.class);
+    String type = msg.get("type");
+
+    System.out.println(type);
+
+    // if (type.equals("ice-candidate")) {
+    //   String iceCandidate = msg.get("iceCandidate");
+
+    //   String id = session.getId();
+
+    //   if (!iceCandidates.containsKey(id)) {
+    //     List<String> list = new ArrayList<>(); 
+    //     list.add(iceCandidate);
+    //     iceCandidates.put(id, list);
+    //   } else {
+    //     iceCandidates.get(id).add(iceCandidate);
+    //   }
+    // } 
+    // else if (type.equals("create-offer")) {
+    //   String offer = msg.get("offer");
+    //   Map<String, Object> map = new ConcurrentHashMap<>();
+    //   map.put("type", "create-offer");
+    //   map.put("offer", offer);
+
+    //   List<String> resultIceCandidates = new ArrayList<>();
+    //   if (iceCandidates.containsKey(session.getId())) {
+    //     resultIceCandidates = iceCandidates.get(session.getId());
+    //   }
+    //   map.put("iceCandidates", resultIceCandidates);
+
+    //   payload = objectMapper.writeValueAsString(map);
+    // }
+
+    for (WebSocketSession s : sessions.values()) {
+      if (!s.getId().equals(session.getId())) {
+        s.sendMessage(new TextMessage(payload));
       }
     }
+  }
+
+  private void handleIceCandidate(String id, String didIOffer, String candidate) {
+    if (didIOffer == "true") {
+      Offer offer = offers.get(id);
+      if (offer != null) {
+        offer.getIceCandidates().add(candidate);
+        offer.getIceCandidates().size();
+
+      } else {
+        System.out.println("Offer is null");
+      }
+    } else if (didIOffer == "false") {
+
+    }
+  }
+
+  private void addNewOffers(String offer, String id) {
+    Offer newOffer = new Offer(id, offer);
+    offers.put(id, newOffer);
   }
 
   @Override
   public void afterConnectionEstablished(WebSocketSession session) throws Exception {
     System.out.println("New Session started");
     sessions.put(session.getId(), session);
+
+    Map<String, String> map = new ConcurrentHashMap<>();
+    map.put("type", "new-user");
+    map.put("id", session.getId());
+
+    try {
+      String jsonString = objectMapper.writeValueAsString(map);
+      System.out.println(jsonString);
+      session.sendMessage(new TextMessage(jsonString));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
--- webRTC/src/main/resources/static/js/ConnectionHandler.js
+++ webRTC/src/main/resources/static/js/ConnectionHandler.js
@@ -1,22 +1,136 @@
 export class ConnectionHandler {
   #signalingServer;
   #localStream;
+  #remoteStream;
+  #id;
+  #configuration;
+  #peerConnection;
+
+  #localVideo;
+  #remoteVideo;
+  #didIOffer;
 
   constructor() {
     this.#signalingServer = new WebSocket('ws://localhost:8080/socket');
-    this.#localStream;
+    this.#localVideo = document.getElementById('local-video');
+    this.#remoteVideo = document.getElementById('remote-video');
+    this.#didIOffer = false;
+    this.#configuration = {
+      iceServers: [
+        {
+          urls: [
+            'stun:stun.l.google.com:19302',
+            'stun:stun1.l.google.com:19302'
+          ]
+        }
+      ]
+    };
+    this.#signalingServerMessages();
+  }
+
+  async #signalingServerMessages() {
+    this.#signalingServer.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      switch (data.type) {
+        case 'new-user':
+          this.#id = data.id;
+          break;
+        case 'ice-candidate':
+          this.#addIceCandidate(data.iceCandidate);
+          break;
+        case 'create-offer':
+          this.#answerOffer(data.offer, data.iceCandidates);
+          break;
+        default:
+          break;
+      }
+    };
+  }
+
+  async call() {
+    await this.#getUserMedia();
+    await this.#createPeerConnection();
+
+    try {
+      console.log("Creating offer...");
+      const offer = await this.#peerConnection.createOffer();
+      this.#didIOffer = true;
+      this.#peerConnection.setLocalDescription(offer);
+      this.#signalingServer.send(JSON.stringify({
+        type: 'create-offer',
+        offer: offer,
+      }));
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async #getUserMedia() {
-    return new Promise( async (resolve, reject) => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        this.#localStream = stream;
-        resolve();
-      } catch (err) {
-        console.log(err);
-        reject();
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      this.#localVideo.srcObject = stream;
+      this.#localStream = stream;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async #createPeerConnection(offerObj) {
+    if (this.#signalingServer) {
+      this.#peerConnection = new RTCPeerConnection(this.#configuration);
+      this.#remoteStream = new MediaStream();
+      this.#remoteVideo.srcObject = this.#remoteStream;
+
+      this.#localStream.getTracks().forEach(track => {
+        this.#peerConnection.addTrack(track, this.#localStream);
+      });
+
+      this.#peerConnection.addEventListener("signalingstatechange", (event) => {
+        console.log(event);
+        console.log(this.#peerConnection.signalingState);
+      });
+
+      this.#peerConnection.addEventListener('icecandidate', (event) => {
+        console.log('Ice Candidate found');
+        console.log(event);
+
+        if (event.candidate) {
+          this.#signalingServer.send(JSON.stringify({
+            type: 'ice-candidate',
+            iceCandidate: event.candidate
+          }));
+        }
+      });
+
+      if (offerObj) {
+        await this.#peerConnection.setRemoteDescription(offerObj.offer);
       }
-    });
+    }
+  }
+
+  async #answerOffer(offerObj, iceCandidates) {
+    // console.log(offerObj, iceCandidates);
+    // this.#addIceCandidate(iceCandidates);
+    // await this.#getUserMedia();
+    // await this.#createPeerConnection(offerObj);
+    // const answer = await this.#peerConnection.createAnswer({});
+    // await this.#peerConnection.setLocalDescription(answer);
+
+    // console.log(offerObj);
+    // console.log(answer);
+
+    // offerObj.answer = answer;
+
+    // const offerIceCandidates = await 
+  }
+
+  #addIceCandidate(iceCandidate) {
+    console.log(iceCandidate);
+    this.#peerConnection.addIceCandidate(iceCandidate);
+
+    // for (let i = 0; i < iceCandidates.length; i++) {
+    //   this.#peerConnection.addIceCandidate(iceCandidates[i]);
+    // }
+    console.log('Ice Candidate added');
   }
 }
